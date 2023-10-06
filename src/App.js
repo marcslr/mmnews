bfc4977bc4f74d2e043c0ed86ab532a571164491
marcslr <sainthilaire.marcjoel@gmail.com>
2023-10-06 12:24:11 +0200

    actualiser

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import "../src/styles/general.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 import Navbar from "../src/components/Navbar.js";
-import Acceuil from "../src/pages/Acceuil.js";
-import Contact from "../src/pages/Contact.js";
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <Route path="/Acceuil" exact component={Acceuil} />
-        <Route path="/Contact" component={Contact} />
-        {/* Ajoutez d'autres routes ici pour vos autres pages */}
-      </div>
+      <Navbar />
+      <div className="App"></div>
     </Router>
   );
 }
